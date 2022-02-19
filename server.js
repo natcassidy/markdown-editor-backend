057dc35f8a3e44092c80052684d1343a4bde5232
natcassidy <nat.cassidy865@gmail.com>
2022-02-19 17:56:39 -0400

    2nd fix on /new-documents query

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ const authenticateToken = (req, res, next) => {
 
 app.post('/new-document', authenticateToken, (req, res) => {
     console.log('New document')
-    con.query(`INSERT INTO document (username, title, content) VALUES ('${req.username}', '${req.body.title}', '${req.body.content}' RETURNING *)`, (err, result) => {
+    con.query(`INSERT INTO document (username, title, content) VALUES ('${req.username}', '${req.body.title}', '${req.body.content}') RETURNING *`, (err, result) => {
         if (err) throw err
         console.log('record inserted\n\n')
         console.log('result from insertion: ', result)
